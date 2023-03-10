b07bd46d703b33cb74d363b73a9a910d5fc2a19d
Ryan Morgan <Zerkyn.55@gmail.com>
2023-03-10 11:49:08 -0700

    added a working GET fortunes button

---
+++ server/controller.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+
+        res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = [
+            'A lifetime of happiness lies ahead of you.',
+            'Everywhere you choose to go, friendly faces will greet you.',
+            'It is worth reviewing some old lessons.',
+            'Practice makes perfect.',
+            'Staying close to home is going to be best for your morale today'
+        ]
+        let randomIndex = Math.floor(Math.random() * fortunes.length)
+        let randomFortune = fortunes[randomIndex]
+        res.status(200).send(randomFortune)
+    },
+
+}
